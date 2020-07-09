463d2cfed375da2853a351b50d361024991e0a06
Vincent Nguyen <vtn28@drexel.edu>
2020-07-09 16:35:49 -0400

    Update hero section and metadata

--- js/bg.js
+++ js/bg.js
@@ -1,22 +1,29 @@
 const MAX_SHAPES = 30;
-const MAX_WIDTH = 10;
-const MIN_DURATION = 10;
-const MAX_DURATION = 20;
+const MIN_WIDTH = 3;
+const MAX_WIDTH = 12;
+const MIN_DURATION = 6;
+const MAX_DURATION = 18;
+const MIN_X_START = 0;
+const MAX_X_START = 100;
+const MIN_Y_START = -100;
+const MAX_Y_START = 0;
 
 function generateBackgroundObjects() {
     let background = document.querySelector("#animated-bg");
     for (let i = 0; i < MAX_SHAPES; i++) {
         let object = document.createElement('i');
-        let width = Math.random() * MAX_WIDTH;
-        let duration = Math.random() * (MAX_DURATION - MIN_DURATION) + MIN_DURATION;
-        let x = Math.floor(Math.random() * 100);
-        let y = Math.floor(Math.random() * 100);
+        let width = Math.floor(Math.random() * (MAX_WIDTH - MIN_WIDTH) + MIN_WIDTH);
+        let duration = Math.floor(Math.random() * (MAX_DURATION - MIN_DURATION) + MIN_DURATION);
+        let opacity = Math.random();
+        let x = Math.floor(Math.random() * (MAX_X_START - MIN_X_START) + MIN_X_START);
+        let y = Math.floor(Math.random() * (MAX_Y_START - MIN_Y_START) + MIN_Y_START);
 
         object.style.left = x + 'vw';
         object.style.top = -y + 'vh';
 
         object.style.borderWidth = width + 'rem';
         object.style.animationDuration = duration + 's';
+        object.style.opacity = opacity;
 
         background.appendChild(object);
     }
