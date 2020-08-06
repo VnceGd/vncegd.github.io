c623ab1ff26369694bc97132006d415ef241282f
Vincent Nguyen <vtn28@drexel.edu>
2020-08-06 13:26:15 -0400

    Add theme caching in local storage

--- js/theme.js
+++ js/theme.js
@@ -1,15 +1,28 @@
 const toggleBtn = document.querySelector("#theme-btn");
 var timeSinceToggle = 0;
 
-toggleBtn.addEventListener('click', () => {
+// Toggle theme (light/dark) of document
+function toggleTheme() {
     if (Date.now() - timeSinceToggle < 200) {
         return;
     }
     timeSinceToggle = Date.now();
     if (document.documentElement.hasAttribute('theme')) {
         document.documentElement.removeAttribute('theme');
+        localStorage.removeItem('theme');
     }
     else {
         document.documentElement.setAttribute('theme', 'toggle');
+        localStorage.setItem('theme', 'toggled');
+    }
+}
+
+// Toggle theme if data found in local storage
+document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem('theme') === 'toggled') {
+        toggleTheme();
     }
 });
+
+// Add listener to theme toggle button
+toggleBtn.addEventListener('click', toggleTheme);
